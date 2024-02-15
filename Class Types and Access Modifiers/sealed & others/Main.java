faa79b0bc78e699ae43b046b962ed5e6b81e0f4d
Ebu13 <ebubekir13042003@gmail.com>
2024-02-15 21:38:14 +0300

    new

--- Class Types and Access Modifiers/sealed & others/Main.java
+++ Class Types and Access Modifiers/sealed & others/Main.java
@@ -1,13 +1,20 @@
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        /*
         List<Shape> sekiller=new ArrayList<>();
         sekiller.add(new Daire(5));
         sekiller.add(new Diktorgen(15,25));
         sekiller.add(new Kare(25));
         sekiller.add(new Ucgen(3,4,5));
+        */
+        List<Shape> sekiller=List.of(new Daire(5),
+                new Diktorgen(15,25),
+                new Kare(25),
+                new Ucgen(3,4,5));
+
         System.out.println("Sırası ile alanları");
         for (Shape i:sekiller){
             System.out.println(i.getAlan());
